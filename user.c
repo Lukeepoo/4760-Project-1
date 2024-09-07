030778fe4d21b4aa5db70dbb854381e970716640
Lukeepoo <lucas.lovellette@gmail.com>
2024-09-06 23:43:05 -0500

    Created oss.c and user.c
    Wrote the skeleton for oss.c and user.c

---
+++ user.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    // Print a simple message to verify the program runs
+    printf("User: Starting up\n");
+
+    // Placeholder for future logic
+
+    return 0;
+}
