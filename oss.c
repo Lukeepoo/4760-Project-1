afa454aaa34e5d6c69a5a35d587c8d87c7498e16
Lukeepoo <lucas.lovellette@gmail.com>
2024-09-07 23:02:55 -0500

    Update oss.c

--- oss.c
+++ oss.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> // For getopt
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    // Default values
     int num_processes = 5;
     int max_simultaneous = 3;
     int iterations = 10;
 
-    // Parsing command-line arguments
     int opt;
     while ((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
         switch (opt) {
@@ -30,12 +30,43 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // For testing, print out the values
     printf("Number of processes: %d\n", num_processes);
     printf("Max simultaneous processes: %d\n", max_simultaneous);
     printf("Iterations per process: %d\n", iterations);
 
-    // Placeholder for future logic to fork and exec user processes
+    // Fork and Exec logic
+    int active_processes = 0;
+    for (int i = 0; i < num_processes; i++) {
+        if (active_processes >= max_simultaneous) {
+            wait(NULL); // Wait for any child to finish
+            active_processes--;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            // In the child process
+            char iter_str[10];
+            sprintf(iter_str, "%d", iterations);
+            execl("./user", "user", iter_str, (char*)NULL);
+            perror("execl failed"); // This will only run if execl fails
+            exit(1);
+        }
+        else if (pid > 0) {
+            // In the parent process
+            active_processes++;
+        }
+        else {
+            // Fork failed
+            perror("fork failed");
+            exit(1);
+        }
+    }
+
+    // Wait for all remaining children to finish
+    while (active_processes > 0) {
+        wait(NULL);
+        active_processes--;
+    }
 
     return 0;
 }
